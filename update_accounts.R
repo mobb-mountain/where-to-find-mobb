607dc57919b21f591a39e408b8e8581928d405b7
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2025-07-10 02:06:34 -0600

    Updates

--- update_accounts.R
+++ update_accounts.R
@@ -37,11 +37,13 @@ df <- read_sheet(sheet_url) |>
   janitor::clean_names() |>
   rename(
     account = account,
+    google_url = google_maps_url,
+    website_url = website,
     address = address_as_shown_in_google,
     products = products_carried_currently,
     type = account_type
   ) |>
-  select(account, address, products, type)
+  select(account, google_url, website_url, address, products, type)
 
 # Transform to long-form and map products/types
 df_long <- df |>
@@ -67,6 +69,8 @@ df_mapped <- df |>
 
 # Add Mobb Mountain tasting room
 mobb <- tibble(account = "Mobb Mountain Distillers",
+               google_url = "https://maps.app.goo.gl/wMU7LySz7QERh8a86",
+               website_url = "https://www.mobbmountain.com/",
                address = "400 Linden St, Fort Collins, CO 80524",
                easy_type = "Tasting Room",
                easy_products = "Vodka, Bourbon, Gin, Limoncello, Rye, Genever, Single Malt Whiskey (limited release)")
@@ -77,19 +81,49 @@ df_mapped <- bind_rows(df_mapped, mobb)
 df_geocoded <- df_mapped |> filter(!is.na(address))
 coords <- geocode(df_geocoded$address, output = "latlon", source = "google")
 df_geo <- bind_cols(df_geocoded, coords) %>%
-  drop_na(lon, lat)
+  drop_na(products, type, lon, lat)
 
 # Convert to sf for GeoJSON export
 df_sf <- st_as_sf(df_geo, coords = c("lon", "lat"), crs = 4326)
 
-# Optional: Format popup field
+# Format popup field
 df_sf <- df_sf |>
-  mutate(popup = paste0(
-    "<strong>", account, "</strong><br>",
-    address, "<br>",
-    "<em>", easy_type, "</em><br>",
-    "Products: ", easy_products
-  )) |>
+  mutate(
+    account_link = if_else(
+      !is.na(website_url) & website_url != "",
+      paste0(
+        "<a href='", website_url, "' target='_blank'>", account,
+        "<img title='Open website' src='styles/up-right-from-square-solid.svg' style='width:12px;height:12px;margin-left:4px;vertical-align:middle;' />",
+        "</a>"
+      ),
+      account
+    ),
+    maps_link = if_else(
+      !is.na(google_url) & google_url != "",
+      paste0(
+        "<a href='", google_url, "' target='_blank'>View on Google Maps",
+        "<img title='Open in Google Maps' src='styles/map-solid.svg' style='width:14px;height:14px;margin-left:4px;vertical-align:middle;' />",
+        "</a>"
+      ),
+      ""
+    ),
+    type_icon = paste0(
+      "<img src='styles/", 
+      case_when(
+        easy_type == "Tasting Room" ~ "crow.svg",
+        easy_type == "Bar/Restaurant/Venue" ~ "glass-cocktail.svg",
+        easy_type == "Liquor Store" ~ "bottle-wine.svg",
+        TRUE ~ "bottle-wine.svg"
+      ), 
+      "' style='width:16px;height:16px;margin-left:8px;vertical-align:middle;' />"
+    ),
+    popup = paste0(
+      "<strong>", account_link, "</strong><br>",
+      "<em>", easy_type, "</em>", "<br>",
+      "Products: ", easy_products, "<br>",
+      maps_link
+    )
+  ) |>
   select(account, address, easy_type, easy_products, popup, geometry)
 
 # Save to GeoJSON
